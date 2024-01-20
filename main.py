d1256cbf6934a94b68ce109ff3f8ca5f8948bb51
Pablojea <pablo_ojea@outlook.com>
2024-01-20 18:31:56 +0100

    implements mockup sensor data and STOP/RESUME capabilities

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import argparse
 import nats
+import numpy as np
 
 async def main():
 
@@ -35,14 +36,30 @@ async def main():
 
     async def message_handler(msg):
         data = msg.data.decode()
-        print(data)
+
+        nonlocal send
+        if data == "STOP":
+            send = False
+        if data == "RESUME":
+            send = True
 
 
     nats_conection = await nats.connect('nats://localhost:4222')
     sensor_control_sub = await nats_conection.subscribe("ir_sensor.control", cb=message_handler)
+    rng = np.random.default_rng()
+
+    send = True
 
     while True:
-        await nats_conection.publish('ir_sensor.data', b'Dummy Data')
+
+        if send and sensor_type == "mockup":
+            data_package = rng.integers(low=data_range[0], high=data_range[1], size=(64), dtype=np.int16)
+            data_string = np.array2string(data_package)
+            await nats_conection.publish('ir_sensor.data', data_string.encode('UTF-8'))
+
+        elif send and sensor_type == "real":
+            pass
+
         await asyncio.sleep(interval)
 
 if __name__ == '__main__':
