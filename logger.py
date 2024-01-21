5207967944effaef3bd5a75c7172f390dc540260
Pablojea <pablo_ojea@outlook.com>
2024-01-21 13:03:26 +0100

    implements logger

---
+++ logger.py
@@ -0,0 +1,30 @@
+import logging
+import time
+
+class Logger:
+
+    def __init__(self):
+
+        logging.basicConfig(filename='info.log',
+                            encoding='utf-8',
+                            filemode='a',
+                            level=logging.INFO,
+                            format='%(asctime)s - %(levelname)s - %(message)s')
+        self.packages_sent = 0
+        self.start_time = time.time()
+        logging.info('Application running.\n')
+
+    def data_sent(self, message):
+        logging.info('data package with following payload: ' + message + '\n')
+        self.packages_sent += 1
+
+    def paused(self):
+        uptime = time.time() - self.start_time
+        logging.info(f'Application paused   uptime: {uptime}s   packages sent: {self.packages_sent}\n')
+        self.start_time = time.time()
+
+    def resumed(self):
+        logging.info('Application resumed.\n')
+
+    def not_implemented(self):
+        logging.info('real sensor was selected, not implemented.\n')
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import argument_parser
 import asyncio
+import logger
 import nats
 import numpy as np
+
 
 async def main():
 
     async def message_handler(msg):
         data = msg.data.decode()
         nonlocal send
-        if data == "STOP":
+        if data == 'PAUSE' or data == 'R':
             send = False
-        if data == "RESUME":
+            log.paused()
+        if data == 'RESUME' or data == 'R':
             send = True
+            log.resumed()
 
+
+    log = logger.Logger()
     (sensor_type , interval, data_range) = argument_parser.parse()
 
     nats_conection = await nats.connect('nats://localhost:4222')
@@ -26,10 +32,11 @@ async def main():
         if send and sensor_type == "mockup":
             data_package = rng.integers(low=data_range[0], high=data_range[1], size=(64), dtype=np.int16)
             data_string = np.array2string(data_package)
+            log.data_sent(data_string)
             await nats_conection.publish('ir_sensor.data', data_string.encode('UTF-8'))
 
         elif send and sensor_type == "real":
-            pass
+            log.not_implemented()
 
         await asyncio.sleep(interval)
 
